f4d921aae48819b79e9bea994985d579473b0e4f
drifkin123 <davidrifkin1@icloud.com>
2025-04-21 09:31:18 -0700

    Use class componentn for App

---
+++ src/data/features.ts
@@ -0,0 +1,35 @@
+export const learnerFeatures = [
+    {
+        title: 'Learn',
+        description: 'With over 1 trillion simulated student and hundreds of pre-designed dynamic simulations included at no charge, leaners at all stages of the career journey can practice decision-making and addressing the needs of diverse learners and classroom situations.',
+        video: '/vids/learn_learn.mp4'
+    },
+    {
+        title: 'Practice',
+        description: 'simSchool offers 100% asynchronous, real-time, unlimited learning. Users may access the simulations whenever they are ready to practice and learn. No need for scheduling and no limits to use. And simSchool\'s Support Team is ready and waiting to provide support 7 days a week.',
+        video: '/vids/learn_practice.mp4'
+    },
+    {
+        title: 'Improve',
+        description: 'In simSchool real-time feedback and personalized reports are central to the learning process. For every 15 minutes of decision-making simSchool provides observations on learner alignment to teaching effectiveness standards, student academic and emotional outcomes, and equitable practice.',
+        video: '/vids/learn_improve.mp4'
+    }
+];
+
+export const institutionFeatures = [
+    {
+        title: 'Observe',
+        description: 'simSchool provides dashboards and reports for instructors, coaches, and mentors to observe the real-time progress of learners in completing content and achieving growth in benchmarked standards. No other simulation platform provides the robust reporting found in simSchool',
+        video: '/vids/inst_observe.mov'
+    },
+    {
+        title: 'Report',
+        description: 'Institutions use the rich, granular data available in simSchool to inform local reports and inform accreditation and state compliance.',
+        video: '/vids/inst_report.mov'
+    },
+    {
+        title: 'Plan',
+        description: 'No other simulation platform enables administrators to observe pre and in-service educator proficiency through the lenses of local, state, and national effectiveness standards. By viewing reports at the individual learner, as well as cohort and institutional level, administrators gain real-time insight into opportunities for improvement and educator development.',
+        video: '/vids/inst_plan.mp4'
+    }
+];
